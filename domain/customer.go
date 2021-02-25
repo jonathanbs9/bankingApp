00c13bc2ca03ca568cc0db27a7f78868e5ab7fa7
Jonathan Brull Schroeder <jonathans@avalith.net>
2021-02-25 11:18:45 -0300

    #12 - implementing SQLX

--- domain/customer.go
+++ domain/customer.go
@@ -3,13 +3,13 @@ package domain
 import "github.com/jonathanbs9/bankingApp/errs"
 
 type Customer struct {
-	Id          string `json:"id"`
-	FirstName   string `json:"first_name"`
-	LastName    string `json:"last_name"`
-	DateOfBirth string `json:"date_of_birth"`
-	City        string `json:"city"`
-	ZipCode     string `json:"zip_code"`
-	Status      bool  `json:"status"`
+	Id          string `db:"customer_id",json:"id"`
+	FirstName   string `db:"first_name",json:"first_name"`
+	LastName    string `db:"last_name",json:"last_name"`
+	DateOfBirth string `db:"date_birth",json:"date_of_birth"`
+	City        string `db:"city",json:"city"`
+	ZipCode     string `db:"zipcode",json:"zip_code"`
+	Status      bool   `db:"status",json:"status"`
 }
 
 type CustomerRepository interface {
--- domain/customerRepositoryDB.go
+++ domain/customerRepositoryDB.go
@@ -3,6 +3,7 @@ package domain
 import (
 	"database/sql"
 	_ "github.com/go-sql-driver/mysql"
+	"github.com/jmoiron/sqlx"
 	"github.com/jonathanbs9/bankingApp/errs"
 	"github.com/jonathanbs9/bankingApp/logger"
 	"log"
@@ -10,68 +11,55 @@ import (
 )
 
 type CustomerRepositoryDb struct {
-	client *sql.DB
+	client *sqlx.DB
 }
 
 func (d CustomerRepositoryDb) FindAll(status string) ([]Customer, *errs.AppError) {
-	/*c, err := sql.Open("mysql", "root:@tcp(locahost:3306)/banking")
-	if err != nil {
-		log.Println("Error al conectar a la base de datos  => " + err.Error())
-		return nil, errs.NewUnexpectedError("Error inesperado en la base de datos")
-	}
-	c.SetConnMaxLifetime(time.Second * 5)
-	c.SetMaxOpenConns(10)
-	c.SetMaxIdleConns(10)*/
-	var rows  *sql.Rows
+	//var rows  *sql.Rows
 	var err error
+	customers := make([]Customer, 0)
 
-	if status == ""{
+	if status == "" {
 		query := "select customer_id, first_name, last_name, date_birth, city, zipcode, status from customers"
-		rows, err = d.client.Query(query)
+		err = d.client.Select(&customers, query)
+		//rows, err = d.client.Query(query)
 	} else {
 		query := "select customer_id, first_name, last_name, date_birth, city, zipcode, status from customers where status = ?"
-		rows, err = d.client.Query(query, status)
+		// 1 destination, 2 query, 3 argument
+		err = d.client.Select(&customers, query, status)
+		//rows, err = d.client.Query(query, status)
 	}
 
 	//rows, err = d.client.Query(query)
 	if err != nil {
-		logger.Error("No se pueden obtener resultados (GetAllCustomers) de la BD => \n " + err.Error())
+		logger.Error("No se pueden obtener resultados (GetAllCustomers) de la BD => ")
+		logger.Error(err.Error())
 		return nil, errs.NewUnexpectedError("Error inesperado en la base de datos")
-	}
-
-	customers := make([]Customer, 0)
-	for rows.Next() {
-		var c Customer
-		err := rows.Scan(&c.Id, &c.FirstName, &c.LastName, &c.DateOfBirth, &c.City, &c.ZipCode, &c.Status)
-		if err != nil {
-			logger.Error("Error al scanear customers => " + err.Error())
-			return nil, errs.NewUnexpectedError("Error inesperado en la base de datos")
-		}
-		customers = append(customers, c)
 	}
 	return customers, nil
 }
 
+// Func GetCustomerById
 func (d CustomerRepositoryDb) GetCustomerById(id string) (*Customer, *errs.AppError) {
-	// Hacemos una llamada a la base de datos
+	// Query
 	customerSql := "select customer_id, first_name, last_name, city, zipcode, date_birth, status from customers where customer_id = ?"
-	row := d.client.QueryRow(customerSql, id)
 	var c Customer
+	// Hacemos una llamada a la base de datos, obtenemos en c un customer. Si no trae nada devuelve un error
+	err := d.client.Get(&c, customerSql, id)
 
-	err:= row.Scan(&c.Id, &c.FirstName, &c.LastName, &c.City, &c.ZipCode, &c.DateOfBirth, &c.Status)
-	if err != nil{
-		if err == sql.ErrNoRows{
-			return nil, errs.NewNotFoundError("Cliente no encontrado")
+	if err != nil {
+		if err == sql.ErrNoRows {
+			return nil, errs.NewNotFoundError("Cliente no encontrado => "+ sql.ErrNoRows.Error())
 		} else {
 			logger.Error("Error al buscar un cliente => " + err.Error())
-			return nil , errs.NewUnexpectedError("Error inesperado en la base de datos")
+			return nil, errs.NewUnexpectedError("Error inesperado en la base de datos")
 		}
 	}
 	return &c, nil
 }
 
 func NewCustomerRepositoryDb() CustomerRepositoryDb {
-	client, err := sql.Open("mysql", "root:@tcp(localhost:3306)/banking")
+	client, err := sqlx.Open("mysql", "root:@tcp(localhost:3306)/banking")
 	if err != nil {
 		log.Fatal("Error al conectar a la base de datos => " + err.Error())
 	}
